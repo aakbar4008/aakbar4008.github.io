cadd7a4b5880bf9d9a08dfd1c7e871cb9f7c332f
aliakbar-tw <ali.akbar@thoughtworks.com>
2026-06-14 01:26:07 -0400

    initial commit/first page of website

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="bg-gray-100 flex-1">
+      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >hello</h1>}
+      
+    </main>
+  );
+}
