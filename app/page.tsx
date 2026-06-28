bdea5fa724917f9e9473a0bc68c71edce964d78f
aakbar4008 <aakbar4008@gmail.com>
2026-06-28 15:10:12 -0600

    page updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 flex-1">
-      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >hello</h1>}
-      
+      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >Home</h1>}
+      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >Projects</h1>}
+      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >Blog/Writeups</h1>}
+      {<h1 className="text-3xl font-bold text-blue-600 mb-8" >Helpful Resources</h1>}
     </main>
   );
 }
